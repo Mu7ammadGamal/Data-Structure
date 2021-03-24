3cea0c13bc92884c68773044ed9110b8449c9256
Muhammad Gamal <47898196+Mu7ammadGamal@users.noreply.github.com>
2021-03-24 17:41:39 +0200

    Add files via upload

---
+++ C/Queue/Array Based/queueArray.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int QDT; // QDT --> Queue Data Type
+#define MAX_QUEUE 10
+#include "queueArray.h"
+
+void userDispQueue(QDT val){
+    printf(" %d /",val);
+}
+
+
+int main()
+{
+    Queue q;
+    QDT val;
+    int c;
+
+    do{
+        printf("Select choice: \n\n");
+        printf("1- Initialize a QUEUE \n");
+        printf("2- en-QUEUE\n");
+        printf("3- de-QUEUE\n");
+        printf("4- Check Full QUEUE\n");
+        printf("5- Check Empty QUEUE\n");
+        printf("6- Display QUEUE\n");
+        printf("7- Clear QUEUE\n");
+        printf("0- Exit\n");
+        printf("> ");
+        scanf("%d",&c);
+        printf("----------------------------------------------------\n");
+
+        if(!isInitialized(&q)&& c!=1 && c!=0){
+            printf(">> QUEUE isn't INITIALIZED\n");
+            printf("----------------------------------------------------\n");}
+
+
+        else{
+
+            switch(c){
+                case 1: //initialize case
+                    if(!isInitialized(&q)){
+                        initQueue(&q);
+                        printf(">> QUEUE initialized Successfully...\n");}
+                    else{printf(">> QUEUE is already INITIALIZED\n");}
+                    break;
+
+                case 2: //enQueue case
+                    if(!isFull(&q)){
+                        printf("Enter the value: ");
+                        scanf("%d",&val);
+                        enQueue(&q,val);
+                        printf(">> %d appended Successfully...\n",val);}
+                     else {printf(">> QUEUE is FULL\n");}
+                    break;
+
+                case 3: //deQueue case
+                    if((!isEmpty(&q)&& isInitialized(&q)) || isFull(&q)){
+                        printf(">> %d dequeued Successfully...\n", deQueue(&q));}
+                    else{printf(">> QUEUE is EMPTY\n");}
+                    break;
+
+                case 4: //isFull case
+                    isFull(&q)?printf(">> QUEUE is FULL\n"):printf(">> QUEUE isn't FULL\n");
+                    break;
+
+                case 5: //isEmpty case
+                    isEmpty(&q)?printf(">> QUEUE is EMPTY\n"):printf(">> QUEUE isn't EMPTY\n");
+                    break;
+
+                case 6://display case
+                    if((!isEmpty(&q) && isInitialized(&q)) || isFull(&q) ){
+                        dispQueue(&q,&userDispQueue);}
+                    else {printf(">> QUEUE is EMPTY\n");}
+                    break;
+
+                case 7:// clear case
+                    clearQueue(&q);
+                    printf(">> QUEUE Cleared Successfully...\n");
+                break;
+
+                default:
+                    if(c!=0 && c>7)
+                        printf(">> WRONG CHOICE\n");}
+
+
+
+                if(c!=0){
+                    printf(">> QUEUE Size: %d\n",MAX_QUEUE);
+                    if(!isEmpty(&q)){
+                    printf(">> Front position at: %d index\n",getFrontPos(&q));
+                    printf(">> Rear position at: %d index\n",getRearPos(&q));
+                    printf(">> Free Space: %d Blocks\n",getFreeSpace(&q));}}
+                else{printf(">> THANKS, ending..\n");}
+                printf("----------------------------------------------------\n");
+
+
+
+
+
+    }}while(c!=0);
+
+}
